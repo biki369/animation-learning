c882fb4bf4f0ed7424967b3217e458c04bd61ee3
Biki Tirua <bikitirua321@gmail.com>
2024-09-01 22:54:06 +0530

    1-9-24/21:53

--- gsap-anime-react/src/App.jsx
+++ gsap-anime-react/src/App.jsx
@@ -7,24 +7,66 @@ import gsap from "gsap";
 
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   const targetDev = useRef(null)
+
+  // useGSAP(
+  //   () => {
+  //     // gsap code here...
+  //     // gsap.to('.overlay', { x: 360, duration: 6, ease: 'elastic', delay: 1 });
+  //     gsap.to('.overlay', { x: 360, duration: 6, ease: 'elastic', delay: 1 });
+  //   },
+  //   // { scope: targetDev }
+  // );
+
+  // gsap.to( targetDev, { rotate: 360, duration: 6 })
+  // useEffect(() => {
+  //   const element = targetDev.current;
+
+  //   gsap.fromTo(element,
+  //     {
+  //       width: 100,
+  //       opacity: 1,
+  //     },
+  //     {
+  //       opacity: 0,
+  //       width: 0, 
+  //       duration: 1.5, ease: 'power4.inOut'
+  //     }
+  //   );
+
+  //   return () => {
+  //     gsap.to('.overlay', {
+  //       width: 0,
+  //       opacity: 1,
+
+  //       duration: 1.5, ease: 'power4.inOut'
+  //     });
+  //   };
+  // }, []);
+
+  //also try this
 
   useGSAP(
     () => {
-      // gsap code here...
-      gsap.to('.square', { x: 360, duration: 6, ease: 'elastic', delay: 1 });
+      gsap.to('.overlay', {
+        width: 0,
+        opacity: 1,
+
+        duration: 1.5, ease: 'power4.inOut'
+      });
     },
     // { scope: targetDev }
   );
 
-  // gsap.to( targetDev, { rotate: 360, duration: 6 })
+
 
   return (
-    <>
+    < div className="app">
+      <div className="overlay" ref={targetDev} ></div>
       <div >Hello World</div>
       <div className="square" ref={targetDev}></div>
-    </>
+    </div>
   )
 }
 
